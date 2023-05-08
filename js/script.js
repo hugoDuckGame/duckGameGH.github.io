823ab0abcde491c6e6984c0d40b79fb211bf5944
duckGameGH <97944476+duckGameGH@users.noreply.github.com>
2023-05-09 01:09:26 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const loader = document.querySelector("#loading");
+function search() { 
+    document.getElementById("notNestle").style.display = "none";
+    document.getElementById("nestle").style.display = "none";
+    let url = 'https://world.openfoodfacts.org/api/v0/product/' + document.getElementById("numb").value + ".json";
+
+    var obj
+    document.getElementById('spinner').style.display = "block";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      obj = data;
+     })
+    .then(() => {
+    document.getElementById('spinner').style.display = "none";
+    console.log(obj);
+      console.log(obj.product.brands);
+      checkBrands(obj.product.brands);
+      document.getElementById('name').innerHTML = obj.product.category_propertiess;
+     });
+
+} 
+
+function checkBrands(input) {
+    if(input.includes("Nestlé")){
+        document.getElementById("nestle").style.display = "block";
+        document.getElementById("notNestle").style.display = "none";
+    }
+    else{
+        document.getElementById("nestle").style.display = "none";
+        document.getElementById("notNestle").style.display = "block";
+    }
+}
+function init(){
+    document.getElementById("notNestle").style.display = "none";
+    document.getElementById("nestle").style.display = "none";
+    document.getElementById('spinner').style.display = "none"; 
+}
+
+
+
+
